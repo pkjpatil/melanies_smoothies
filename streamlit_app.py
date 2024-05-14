e38b22083af54005821c4d833e823554bbfa55c0
pkjpatil <162716473+pkjpatil@users.noreply.github.com>
2024-05-15 00:22:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        URL_string = 'https://fruityvice.com/api/fruit/'+fruit_chosen
-        fruityvice_response = requests.get(URL_string)
+        # URL_string = 'https://fruityvice.com/api/fruit/'+fruit_chosen
+        st.subheader=(fruit_chosed + ' Nutrition information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
